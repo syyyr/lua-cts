6370e1e9e7a01851b54ef493b0d8ad635bbad094
Václav Kubernát <sir.venceslas@gmail.com>
2022-05-20 10:05:22 +0200

    Add test for pushinteger

--- tests/stack.cpp
+++ tests/stack.cpp
@@ -3,11 +3,9 @@
 
 #include <lua-cts.hpp>
 
-struct lua_State {
-};
 TEST_CASE("stack")
 {
-    auto mock_state = std::make_unique<lua_State>();
+    auto mock_state = std::unique_ptr<lua_State, decltype(&lua_close)>(luaL_newstate(), lua_close);
 
     DOCTEST_SUBCASE("Empty stack is empty")
     {
@@ -17,5 +15,12 @@ TEST_CASE("stack")
     DOCTEST_SUBCASE("Creating a stack with one int")
     {
         REQUIRE(lua::StackWrapper<lua::Int>(mock_state.get()).stack_size == 1);
+    }
+
+    DOCTEST_SUBCASE("Pushing values")
+    {
+        auto s = lua::StackWrapper<>(mock_state.get()).pushinteger(1);
+        REQUIRE(s.stack_size == 1);
+        (void) s.tointeger<1>([] (int x) {REQUIRE(x == 1);} );
     }
 }
